0a7ffb7e2083b291940e7e59d241904d14e90439
Bram Renting <bramrenting@gmail.com>
2023-03-20 13:57:54 +0100

    updated default testing agent to winner ANL 2022

--- run.py
+++ run.py
@@ -18,7 +18,8 @@ if not RESULTS_DIR.exists():
 settings = {
     "agents": [
         {
-            "class": "agents.boulware_agent.boulware_agent.BoulwareAgent",
+            "class": "agents.ANL2022.dreamteam109_agent.dreamteam109_agent.DreamTeam109Agent",
+            "parameters": {"storage_dir": "agent_storage/DreamTeam109Agent"},
         },
         {
             "class": "agents.template_agent.template_agent.TemplateAgent",
