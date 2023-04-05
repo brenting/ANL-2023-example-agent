6cfc2de07463086d4bc7650d9bcded639c3d5400
Sabina Gradinariu <s.m.gradinariu@student.tudelft.nl>
2023-04-05 11:35:21 +0200

    Ordered the criterions as in the slides, commented the tit for tat.

--- agents/group16_agent/acceptance_strategies/acceptance_strategy.py
+++ agents/group16_agent/acceptance_strategies/acceptance_strategy.py
@@ -3,12 +3,14 @@ from geniusweb.issuevalue.Domain import Domain
 from time import time
 import random
 
+
 class AcceptanceStrategy:
 
     def __init__(self, profile, progress):
         self._profile = profile
         self._progress = progress
         self._last_bid = None
+        self._reservation_value = 0.3
 
     def accept_condition(self, bid: Bid) -> bool:
         if bid is None:
@@ -20,22 +22,25 @@ class AcceptanceStrategy:
         utility = self._profile.getUtility(bid)
 
         # Determine the minimum acceptable utility based on the progress of the negotiation.
-        min_utility = 1.0 - progress
+        min_utility = 1.0 - (progress / 3.0)
 
-        # If this is the first bid, accept it.
-        if self._last_bid is None:
-            self._last_bid = bid
-            return True
-
-        # If the bid is at least as good as the last bid made by us, accept it.
-        if self._profile.getUtility(bid) >= self._profile.getUtility(self._last_bid):
-            self._last_bid = bid
+        if utility >= min_utility:
             return True
 
         # If the bid is close to the minimum acceptable utility, use a random threshold to decide.
-        elif utility >= min_utility * 0.9:
-            return random.random() < 0.5
+        if utility >= min_utility * 0.9:
+            return random.uniform(0, 1) < 0.5
+
+        # Check if the time remaining is less than the ACtime(T) threshold.
+        if progress >= 0.90 and utility >= self._reservation_value:
+            return True
 
         # Otherwise, reject the bid.
         else:
             return False
+
+
+        # # If the bid is at least as good as the last bid made by us, accept it.
+        # if self._profile.getUtility(bid) >= self._profile.getUtility(self._last_bid):
+        #     self._last_bid = bid
+        #     return True
